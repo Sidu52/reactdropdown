c3ea995100009df68150ee97ede6b2b6f27a81ef
Siddhant sharma <hitechsidu992@gmail.com>
2023-05-20 14:39:19 +0530

    Add project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import Dropdown from "./component/dropdown/Dropdown";
 import './App.css';
-
 function App() {
+  // managing the state
+  const [open, setopen] = React.useState(false);
+  // the array which has to be iterated and the data of which has to be shown
+  const arr = ["option1", "option2", "option3", "option4", "option5", "option6", "option7"];
+
+  function handleOpen() {
+    setopen((prevState) => {
+      return !prevState;
+    })
+  }
+  function handleClose() {
+    setopen(false);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <h2>Click on the button</h2>
+      <button className='hovering' onMouseOver={handleOpen} >Hover over this button</button>
+      {open && <div className="element"  >
+        <Dropdown arr={arr} disappear={handleClose} />
+      </div>}
     </div>
   );
 }
